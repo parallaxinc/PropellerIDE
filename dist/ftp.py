5cef8b3cb3cb8388811976400387452e9af2f0aa
Brett Weir <contact@lamestation.com>
2014-11-30 05:24:52 -0800

    Eliminated unneeded code in ftp.py script.

--- dist/ftp.py
+++ dist/ftp.py
@@ -13,15 +13,9 @@ username = raw_input()
 print "Enter password:"
 passwd = getpass.getpass()
 
-def upload(ftp, file):
-    ext = os.path.splitext(file)[1]
-    if ext in (".txt", ".htm", ".html"):
-        ftp.storlines("STOR " + file, open(file))
-    else:
-        ftp.storbinary("STOR " + file, open(file, "rb"), 1024)
-
 ftp = ftplib.FTP(host)
 ftp.login(username,passwd)
 ftp.cwd("/public_html/downloads/"+platform.system())
 
-upload(ftp, filename)
+command = 'STOR ' + filename
+ftp.storbinary(command, open(filename, "rb"), 1024)
