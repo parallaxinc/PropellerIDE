23c1771e921c5a7d90bb2721cbf9aa0cd7e40540
Brett Weir <brett@lamestation.com>
2015-09-13 15:08:50 -0700

    Add dual linux/mac build support to Travis configuration

---
+++ scripts/travis.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# install Qt
+
+case "$PLATFORM" in
+"osx")
+    brew update
+    brew install qt5
+    brew link qt5 --force
+    ;;
+"linux")
+    wget -4 http://lamestation.net/downloads/travis/qt5.5.0-linux-g++.tgz
+    tar xzf qt5.5.0-linux-g++.tgz
+    mv local/ /home/travis/local/
+    export PATH=/home/travis/local/bin:$PATH
+    export LD_LIBRARY_PATH=/home/travis/local/lib:$LD_LIBRARY_PATH
+    ls /home/travis/local/bin
+    ls /home/travis/local/lib
+    ;;
+*)
+    echo "Invalid PLATFORM"
+    exit 1
+    ;;
+esac
+qmake -v
+
+# install packthing
+
+mkdir -p $HOME/local/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:$HOME/local/lib/python2.7/site-packages
+export PATH=$PATH:$HOME/local/bin
+easy_install --prefix=$HOME/local packthing
+packthing -h
