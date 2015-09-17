a5996dd8ab19bd3eb99d64218c262e2d21a46c1e
Brett Weir <brett@lamestation.com>
2015-09-17 15:23:52 -0700

    Make build-dependent file tree listing because tree isn't available in Debian VM.

--- scripts/travis/after_script.sh
+++ scripts/travis/after_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-ls -R build/staging/
-tree 
 
 case "$PLATFORM" in
 "osx")
+    tree build/staging/
     for f in build/staging/mac/PropellerIDE.app/Contents/MacOS/*; do echo $f; otool -L $f; done
     ;;
 "linux")
+    ls -R build/staging/
     for f in build/staging/debian/propelleride/usr/bin/*; do echo $f; ldd $f; done
     ;;
 *)
