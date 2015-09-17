9c451550f2f73ce7289b42b60546ab8ba9306b6f
Brett Weir <brett@lamestation.com>
2015-09-17 15:17:05 -0700

    Fix Mac path and check dynamic linking

--- scripts/travis/after_script.sh
+++ scripts/travis/after_script.sh
@@ -6,8 +6,10 @@ tree
 
 case "$PLATFORM" in
 "osx")
+    for f in build/staging/mac/PropellerIDE.app/Contents/MacOS/*; do echo $f; otool -L $f; done
     ;;
 "linux")
+    for f in build/staging/debian/propelleride/usr/bin/*; do echo $f; ldd $f; done
     ;;
 *)
     echo "Invalid PLATFORM"
