aa81cddec4fae34e5d96e012b7efd214a8981c3b
Brett Weir <brett@lamestation.com>
2015-09-23 03:37:48 -0700

    Generate self-extracting archive in addition to debian package

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -6,10 +6,15 @@ packthing -h
 
 case "$PLATFORM" in
 "osx")
-    packthing -j4 $PACKTHING
+    packthing -j4 dmg
+    mv build/staging/propelleride-*.dmg .
     ;;
 "linux")
-    fakeroot packthing -j4 $PACKTHING
+    packthing -j4 run
+    mv build/staging/propelleride-*.run .
+
+    fakeroot packthing -j4 deb
+    mv build/staging/propelleride-*.deb .
     ;;
 *)
     echo "Invalid PLATFORM"
